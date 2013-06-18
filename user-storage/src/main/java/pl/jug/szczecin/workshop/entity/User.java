2ca35c1de90d72a447d4991e99dbe2253424cfbb
Marcin <kardigen@gmail.com>
2013-06-18 11:00:11 +0200

    User as mongo entity

--- user-storage/src/main/java/pl/jug/szczecin/workshop/entity/User.java
+++ user-storage/src/main/java/pl/jug/szczecin/workshop/entity/User.java
@@ -1,17 +1,34 @@
 package pl.jug.szczecin.workshop.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Document(collection = "users")
 public class User {
 
-    private final String name;
-    private final String surname;
-    private final String email;
-    private final String webpage;
+    @Id
+    private String id;
+
+    private String name;
+    private String surname;
+    private String email;
+    private String webpage;
+
+    public User() {
+    }
 
     public User(String name, String surname, String email, String webpage) {
         this.name = name;
         this.surname = surname;
         this.email = email;
         this.webpage = webpage;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getName() {
@@ -33,10 +50,31 @@ public class User {
     @Override
     public String toString() {
         return "User{" +
+                "id='" + id + '\'' +
                 "name='" + name + '\'' +
                 ", surname='" + surname + '\'' +
                 ", email='" + email + '\'' +
                 ", webpage='" + webpage + '\'' +
                 '}';
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setWebpage(String webpage) {
+        this.webpage = webpage;
     }
 }
